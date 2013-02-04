8bc852418f9c43e18075e69274a4e4ff794b4e75
Peter C. Norton <pn@knewton.com>
2013-02-04 16:29:54 -0500

    Remove the installation of pip via python-distribute.org.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs Python, pip and virtualenv. Includes LWRPs for managing Python packages with `pip` and `virtualenv` isolated Python environments."
-version           "1.2.2"
+version           Time.at(`git --git-dir=#{File.join(File.dirname(__FILE__),'.git')} log --max-count=1 --pretty=format:%ct`.to_i).strftime("1.2.%Y%m%d%H%M")
 
 depends           "build-essential"
 depends           "yum"
